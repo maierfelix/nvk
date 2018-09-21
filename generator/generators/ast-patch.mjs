7ef452338b271b9feafd978949e1a6b6dad93d4d
Felix Maier <xilefmai@gmail.com>
2018-09-21 18:39:24 +0200

    Remove tabs

--- generator/generators/ast-patch.mjs
+++ generator/generators/ast-patch.mjs
@@ -1,30 +1,30 @@
 export default function(ast) {
 
-	let structs = ast.filter(node => node.kind === "STRUCT");
+  let structs = ast.filter(node => node.kind === "STRUCT");
 
-	structs.map(struct => {
+  structs.map(struct => {
 
-		// PATCH:
+    // PATCH:
     // memoryTypes, memoryHeaps
     // to reflect them we need to read from a dynamic length
     // the dynamic length gets attached to the AST node here
-		{
-			let patches = 0;
-			struct.children.map(member => {
-		    if (member.name === "memoryHeaps") {
-		    	member.dynamicLength = "memoryHeapCount";
-		    	patches++;
-		    }
-		    if (member.name === "memoryTypes") {
-		    	member.dynamicLength = "memoryTypeCount";
-		    	patches++;
-		    }
-		  });
-		  if (patches > 0 && patches !== 2) {
-		  	console.error(`Expected patch count of 2 but got ${patches}`);
-		  }
-		}
+    {
+      let patches = 0;
+      struct.children.map(member => {
+        if (member.name === "memoryHeaps") {
+          member.dynamicLength = "memoryHeapCount";
+          patches++;
+        }
+        if (member.name === "memoryTypes") {
+          member.dynamicLength = "memoryTypeCount";
+          patches++;
+        }
+      });
+      if (patches > 0 && patches !== 2) {
+        console.error(`Expected patch count of 2 but got ${patches}`);
+      }
+    }
 
-	});
+  });
 
 };
