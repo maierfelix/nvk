34129c71dff6558ebf975e98bebcfa39c9e68f7b
maierfelix <xilefmai@gmail.com>
2019-02-14 01:08:51 +0100

    Better formating for generated line numbers

--- generator/index.mjs
+++ generator/index.mjs
@@ -223,8 +223,6 @@ async function generateBindings({xml, version, docs, incremental} = _) {
       let result = generateStructs(ast, struct);
       result.includes.map(incl => includes.push(incl));
       if (includes.indexOf(struct.name) <= -1) includes.push({ name: struct.name, include: "" });
-      //writeAddonFile(`${generateSrcPath}/${struct.name}.h`, result.header, "utf-8", true);
-      //writeAddonFile(`${generateSrcPath}/${struct.name}.cpp`, result.source, "utf-8", true);
       files.push({
         name: struct.name,
         header: result.header,
@@ -238,8 +236,6 @@ async function generateBindings({xml, version, docs, incremental} = _) {
     handles.map(handle => {
       let result = generateHandles(ast, handle);
       if (includes.indexOf(handle.name) <= -1) includes.push({ name: handle.name, include: "" });
-      //writeAddonFile(`${generateSrcPath}/${handle.name}.h`, result.header, "utf-8", true);
-      //writeAddonFile(`${generateSrcPath}/${handle.name}.cpp`, result.source, "utf-8", true);
       files.push({
         name: handle.name,
         header: result.header,
@@ -383,7 +379,7 @@ async function generateBindings({xml, version, docs, incremental} = _) {
   console.log(`Generation stats:`);
   console.log(`Total files: ${totalFiles}/${writtenFiles}`);
   console.log(`Total size: ${writtenBytes * 1e-3}kb`);
-  console.log(`Total lines: ${writtenLines}`);
+  console.log(`Total lines: ${(writtenLines).toLocaleString().replace(",", ".")} lines`);
 };
 
 let vkVersion = process.env.npm_config_vkversion;
