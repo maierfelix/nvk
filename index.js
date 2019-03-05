eb7faf0416b6509b5e423adb52bdd6e8cf786ef2
maierfelix <xilefmai@gmail.com>
2019-03-05 15:10:21 +0100

    Throw error when an outdated build is used

--- index.js
+++ index.js
@@ -41,6 +41,13 @@ if (!vkVersion) throw `No vulkan version --vkversion specified!`;
 
 let {platform} = process;
 
+// strictly dissallow older versions
+// TODO: make this less harsh?
+if (pkg.config.OUTDATED.indexOf(vkVersion) > -1) {
+  throw `${vkVersion} is outdated and no longer supported!
+Please use v${pkg.config.POST_DEFAULT_BINDING_VERSION} from now on!`;
+}
+
 const releasePath = `${pkg.config.GEN_OUT_DIR}/${vkVersion}/${platform}/build/Release`;
 const addonLocalPath = `${releasePath}/addon-${platform}.node`;
 const addonPath = path.join(__dirname, addonLocalPath);
@@ -53,8 +60,9 @@ if (!fs.existsSync(addonPath)) {
   process.stderr.write(`You may instead use one of the following bindings:\n`);
   fs.readdirSync(bindingsPath).forEach(dirname => {
     let addon = null;
+    let addonNodePath = bindingsPath + dirname + `/${platform}/build/Release/addon-${platform}.node`;
     try {
-      addon = require(bindingsPath + dirname + `/build/Release/addon-${platform}.node`);
+      addon = require(addonNodePath);
     } catch (e) {
 
     }
