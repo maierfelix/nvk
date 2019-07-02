23c2cf04de504f95534b830ae9f7bd0e58299da0
Felix Maier <xilefmaigmail.com>
2019-07-02 19:11:22 +0200

    Update darwin build for Vulkan 1.1.108

---
+++ generated/1.1.108/darwin/build/Release/.deps/Release/obj.target/addon-darwin/src/index.o.d
@@ -0,0 +1,36 @@
+cmd_Release/obj.target/addon-darwin/src/index.o := c++ '-DNODE_GYP_MODULE_NAME=addon-darwin' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-D_DARWIN_USE_64_BIT_INODE=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DNAPI_DISABLE_CPP_EXCEPTIONS' '-DBUILDING_NODE_EXTENSION' -I/Users/user/.node-gyp/11.14.0/include/node -I/Users/user/.node-gyp/11.14.0/src -I/Users/user/.node-gyp/11.14.0/deps/openssl/config -I/Users/user/.node-gyp/11.14.0/deps/openssl/openssl/include -I/Users/user/.node-gyp/11.14.0/deps/uv/include -I/Users/user/.node-gyp/11.14.0/deps/zlib -I/Users/user/.node-gyp/11.14.0/deps/v8/include -I/Users/user/Documents/GitHub/nvk/node_modules/node-addon-api -I../../../../lib/include -I/Users/user/Documents/Projects/vulkansdk-macos-1.1.108.0/macOS/include  -Os -gdwarf-2 -mmacosx-version-min=10.7 -arch x86_64 -Wall -Wendif-labels -W -Wno-unused-parameter -std=gnu++1y -stdlib=libc++ -fno-rtti -fno-exceptions -std=c++11 -stdlib=libc++ -MMD -MF ./Release/.deps/Release/obj.target/addon-darwin/src/index.o.d.raw   -c -o Release/obj.target/addon-darwin/src/index.o ../src/index.cpp
+Release/obj.target/addon-darwin/src/index.o: ../src/index.cpp \
+  /Users/user/Documents/GitHub/nvk/node_modules/node-addon-api/napi.h \
+  /Users/user/.node-gyp/11.14.0/include/node/node_api.h \
+  /Users/user/.node-gyp/11.14.0/include/node/js_native_api.h \
+  /Users/user/.node-gyp/11.14.0/include/node/js_native_api_types.h \
+  /Users/user/.node-gyp/11.14.0/include/node/node_api_types.h \
+  /Users/user/Documents/GitHub/nvk/node_modules/node-addon-api/napi-inl.h \
+  /Users/user/Documents/GitHub/nvk/node_modules/node-addon-api/napi-inl.deprecated.h \
+  ../../../../lib/include/GLFW/glfw3.h \
+  /Users/user/Documents/Projects/vulkansdk-macos-1.1.108.0/macOS/include/vulkan/vulkan.h \
+  /Users/user/Documents/Projects/vulkansdk-macos-1.1.108.0/macOS/include/vulkan/vk_platform.h \
+  /Users/user/Documents/Projects/vulkansdk-macos-1.1.108.0/macOS/include/vulkan/vulkan_core.h \
+  ../../../../lib/include/GLFW/glfw3native.h \
+  /Users/user/Documents/Projects/vulkansdk-macos-1.1.108.0/macOS/include/vulkan/vulkan_macos.h \
+  ../src/calls.h ../src/utils.h ../src/enums.h ../src/window.h \
+  ../src/memoryLayouts.h
+../src/index.cpp:
+/Users/user/Documents/GitHub/nvk/node_modules/node-addon-api/napi.h:
+/Users/user/.node-gyp/11.14.0/include/node/node_api.h:
+/Users/user/.node-gyp/11.14.0/include/node/js_native_api.h:
+/Users/user/.node-gyp/11.14.0/include/node/js_native_api_types.h:
+/Users/user/.node-gyp/11.14.0/include/node/node_api_types.h:
+/Users/user/Documents/GitHub/nvk/node_modules/node-addon-api/napi-inl.h:
+/Users/user/Documents/GitHub/nvk/node_modules/node-addon-api/napi-inl.deprecated.h:
+../../../../lib/include/GLFW/glfw3.h:
+/Users/user/Documents/Projects/vulkansdk-macos-1.1.108.0/macOS/include/vulkan/vulkan.h:
+/Users/user/Documents/Projects/vulkansdk-macos-1.1.108.0/macOS/include/vulkan/vk_platform.h:
+/Users/user/Documents/Projects/vulkansdk-macos-1.1.108.0/macOS/include/vulkan/vulkan_core.h:
+../../../../lib/include/GLFW/glfw3native.h:
+/Users/user/Documents/Projects/vulkansdk-macos-1.1.108.0/macOS/include/vulkan/vulkan_macos.h:
+../src/calls.h:
+../src/utils.h:
+../src/enums.h:
+../src/window.h:
+../src/memoryLayouts.h:
