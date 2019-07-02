ae74f4f9347a5edb0ff8f607aa72c7c863b4ec49
Felix Maier <xilefmai@gmail.com>
2019-07-02 19:46:18 +0200

    Update Linux build to Vulkan 1.1.108

---
+++ generated/1.1.108/linux/build/Release/.deps/Release/obj.target/addon-linux/src/index.o.d
@@ -0,0 +1,32 @@
+cmd_Release/obj.target/addon-linux/src/index.o := g++ '-DNODE_GYP_MODULE_NAME=addon-linux' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I/home/user/.node-gyp/11.10.1/include/node -I/home/user/.node-gyp/11.10.1/src -I/home/user/.node-gyp/11.10.1/deps/openssl/config -I/home/user/.node-gyp/11.10.1/deps/openssl/openssl/include -I/home/user/.node-gyp/11.10.1/deps/uv/include -I/home/user/.node-gyp/11.10.1/deps/zlib -I/home/user/.node-gyp/11.10.1/deps/v8/include -I/home/user/Documents/GitHub/nvk/node_modules/node-addon-api -I../../../../lib/include -I/home/user/vulkan/1.1.108.0/x86_64/include  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -m64 -std=c++11 -fexceptions -Wno-switch -Wno-unused -Wno-uninitialized -O3 -fno-omit-frame-pointer -fno-rtti -fno-exceptions -std=gnu++1y -std=c++11 -fexceptions -Wno-switch -Wno-unused -Wno-uninitialized -MMD -MF ./Release/.deps/Release/obj.target/addon-linux/src/index.o.d.raw   -c -o Release/obj.target/addon-linux/src/index.o ../src/index.cpp
+Release/obj.target/addon-linux/src/index.o: ../src/index.cpp \
+ /home/user/Documents/GitHub/nvk/node_modules/node-addon-api/napi.h \
+ /home/user/.node-gyp/11.10.1/include/node/node_api.h \
+ /home/user/.node-gyp/11.10.1/include/node/js_native_api.h \
+ /home/user/.node-gyp/11.10.1/include/node/js_native_api_types.h \
+ /home/user/.node-gyp/11.10.1/include/node/node_api_types.h \
+ /home/user/Documents/GitHub/nvk/node_modules/node-addon-api/napi-inl.h \
+ /home/user/Documents/GitHub/nvk/node_modules/node-addon-api/napi-inl.deprecated.h \
+ ../../../../lib/include/GLFW/glfw3.h \
+ /home/user/vulkan/1.1.108.0/x86_64/include/vulkan/vulkan.h \
+ /home/user/vulkan/1.1.108.0/x86_64/include/vulkan/vk_platform.h \
+ /home/user/vulkan/1.1.108.0/x86_64/include/vulkan/vulkan_core.h \
+ ../src/calls.h ../src/utils.h ../src/enums.h ../src/window.h \
+ ../src/memoryLayouts.h
+../src/index.cpp:
+/home/user/Documents/GitHub/nvk/node_modules/node-addon-api/napi.h:
+/home/user/.node-gyp/11.10.1/include/node/node_api.h:
+/home/user/.node-gyp/11.10.1/include/node/js_native_api.h:
+/home/user/.node-gyp/11.10.1/include/node/js_native_api_types.h:
+/home/user/.node-gyp/11.10.1/include/node/node_api_types.h:
+/home/user/Documents/GitHub/nvk/node_modules/node-addon-api/napi-inl.h:
+/home/user/Documents/GitHub/nvk/node_modules/node-addon-api/napi-inl.deprecated.h:
+../../../../lib/include/GLFW/glfw3.h:
+/home/user/vulkan/1.1.108.0/x86_64/include/vulkan/vulkan.h:
+/home/user/vulkan/1.1.108.0/x86_64/include/vulkan/vk_platform.h:
+/home/user/vulkan/1.1.108.0/x86_64/include/vulkan/vulkan_core.h:
+../src/calls.h:
+../src/utils.h:
+../src/enums.h:
+../src/window.h:
+../src/memoryLayouts.h:
