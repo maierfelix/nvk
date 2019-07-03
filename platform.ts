d8015d6f9d666e7212466f72cb7b6c5aa72cc2a0
maierfelix <xilefmai@gmail.com>
2019-07-03 15:46:25 +0200

    Validate platform in index.js, formatings for platform.ts

--- platform.ts
+++ platform.ts
@@ -1,19 +1,18 @@
-import * as linuxNVK from "./generated/1.1.108/linux";
-import * as darwinNVK from "./generated/1.1.108/darwin";
-import * as win32NVK from "./generated/1.1.108/win32";
+import * as linux from "./generated/1.1.108/linux";
+import * as win32 from "./generated/1.1.108/win32";
+import * as darwin from "./generated/1.1.108/darwin";
 
 let nvk: any;
 switch (process.platform) {
-    case "linux":
-        nvk = linuxNVK;
-        break;
-    case "win32":
-        nvk = win32NVK;
-        break;
-    case "darwin":
-        nvk = darwinNVK;
-        break;
-    default:
-        throw 'This os is not supported by nvk';
-}
+  case "linux":
+    nvk = linux;
+  break;
+  case "win32":
+    nvk = win32;
+  break;
+  case "darwin":
+    nvk = darwin;
+  break;
+};
+
 export default nvk;
