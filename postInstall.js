211aee03ab07325e3c630ee381b9deb685aa18ec
Zeeshan Abid <mail@zeeshan-abid.com>
2019-10-06 16:12:32 +0100

    changed version

--- postInstall.js
+++ postInstall.js
@@ -1,3 +1,4 @@
 const fs = require('fs');
-const vkversion = '1.1.121';
+const vkversion = '1.1.114';
+fs.unlinkSync('./index.d.ts');
 fs.symlinkSync(`./generated/${vkversion}/${process.platform}/index.d.ts`, './index.d.ts');
