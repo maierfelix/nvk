1396d0d6750647deaac6af2a4d9513023e87b759
Zeeshan Abid <mail@zeeshan-abid.com>
2019-10-07 09:29:11 +0100

    Where unlink throws an error if index.d.ts does not exist

--- postInstall.js
+++ postInstall.js
@@ -1,4 +1,8 @@
 const fs = require('fs');
 const vkversion = '1.1.114';
-fs.unlinkSync('./index.d.ts');
-fs.symlinkSync(`./generated/${vkversion}/${process.platform}/index.d.ts`, './index.d.ts');
+try {
+  fs.unlinkSync('./index.d.ts');
+} catch (e) { }
+try {
+  fs.symlinkSync(`./generated/${vkversion}/${process.platform}/index.d.ts`, './index.d.ts');
+} catch (e) { }
