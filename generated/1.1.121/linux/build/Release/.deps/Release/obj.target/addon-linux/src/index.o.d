2c6be96d35a9e366e19cbccdb2560bf323980102
Zeeshan Abid <mail@zeeshan-abid.com>
2019-10-08 12:23:18 +0100

    linux generated 1.1.121 bindings

---
+++ generated/1.1.121/linux/build/Release/.deps/Release/obj.target/addon-linux/src/index.o.d
@@ -0,0 +1,25 @@
+cmd_Release/obj.target/addon-linux/src/index.o := g++ '-DNODE_GYP_MODULE_NAME=addon-linux' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-DV8_DEPRECATION_WARNINGS' '-DV8_IMMINENT_DEPRECATION_WARNINGS' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I/home/zeeshan/.cache/node-gyp/12.9.1/include/node -I/home/zeeshan/.cache/node-gyp/12.9.1/src -I/home/zeeshan/.cache/node-gyp/12.9.1/deps/openssl/config -I/home/zeeshan/.cache/node-gyp/12.9.1/deps/openssl/openssl/include -I/home/zeeshan/.cache/node-gyp/12.9.1/deps/uv/include -I/home/zeeshan/.cache/node-gyp/12.9.1/deps/zlib -I/home/zeeshan/.cache/node-gyp/12.9.1/deps/v8/include -I/home/zeeshan/Documents/Projects/tortuga-library/node_modules/nvk/node_modules/node-addon-api -I../../../../lib/include -I/include  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -m64 -std=c++11 -fexceptions -Wno-switch -Wno-unused -Wno-uninitialized -O3 -fno-omit-frame-pointer -fno-rtti -fno-exceptions -std=gnu++1y -std=c++11 -fexceptions -Wno-switch -Wno-unused -Wno-uninitialized -MMD -MF ./Release/.deps/Release/obj.target/addon-linux/src/index.o.d.raw   -c -o Release/obj.target/addon-linux/src/index.o ../src/index.cpp
+Release/obj.target/addon-linux/src/index.o: ../src/index.cpp \
+ /home/zeeshan/Documents/Projects/tortuga-library/node_modules/nvk/node_modules/node-addon-api/napi.h \
+ /home/zeeshan/.cache/node-gyp/12.9.1/include/node/node_api.h \
+ /home/zeeshan/.cache/node-gyp/12.9.1/include/node/js_native_api.h \
+ /home/zeeshan/.cache/node-gyp/12.9.1/include/node/js_native_api_types.h \
+ /home/zeeshan/.cache/node-gyp/12.9.1/include/node/node_api_types.h \
+ /home/zeeshan/Documents/Projects/tortuga-library/node_modules/nvk/node_modules/node-addon-api/napi-inl.h \
+ /home/zeeshan/Documents/Projects/tortuga-library/node_modules/nvk/node_modules/node-addon-api/napi-inl.deprecated.h \
+ ../../../../lib/include/GLFW/glfw3.h ../src/calls.h ../src/utils.h \
+ ../src/enums.h ../src/window.h ../src/memoryLayouts.h
+../src/index.cpp:
+/home/zeeshan/Documents/Projects/tortuga-library/node_modules/nvk/node_modules/node-addon-api/napi.h:
+/home/zeeshan/.cache/node-gyp/12.9.1/include/node/node_api.h:
+/home/zeeshan/.cache/node-gyp/12.9.1/include/node/js_native_api.h:
+/home/zeeshan/.cache/node-gyp/12.9.1/include/node/js_native_api_types.h:
+/home/zeeshan/.cache/node-gyp/12.9.1/include/node/node_api_types.h:
+/home/zeeshan/Documents/Projects/tortuga-library/node_modules/nvk/node_modules/node-addon-api/napi-inl.h:
+/home/zeeshan/Documents/Projects/tortuga-library/node_modules/nvk/node_modules/node-addon-api/napi-inl.deprecated.h:
+../../../../lib/include/GLFW/glfw3.h:
+../src/calls.h:
+../src/utils.h:
+../src/enums.h:
+../src/window.h:
+../src/memoryLayouts.h:
